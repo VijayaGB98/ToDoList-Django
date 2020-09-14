e1b106f175426e4f484f31e9cc9300000c37aa08
VijayaGB98 <vijaya.gajanan@outlook.com>
2020-09-15 01:08:24 +0400

    Updated Tasklist

--- todolist/todolist_app/models.py
+++ todolist/todolist_app/models.py
@@ -7,8 +7,13 @@ class Task(models.Model):
         ('Normal', 'Normal'),
         ('Low', 'Low'),
     )
+    STATUS = (
+        ('Incomplete', 'Incomplete'),
+        ('Complete', 'Complete'),
+    )
     task_name = models.CharField(max_length=500)
-    priority = models.CharField(max_length=255, choices=PRIORITY)
+    priority = models.CharField(max_length=255, choices=PRIORITY, default='Low')
+    status = models.CharField(max_length=255, choices=STATUS, default='Incomplete')
     date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- todolist/todolist_app/views.py
+++ todolist/todolist_app/views.py
@@ -3,4 +3,7 @@ from .models import *
 
 # Render homepage
 def home(request):
-    return render(request, 'todolist_app/main.html')
+    incomplete_tasks = Task.objects.filter(status='Incomplete')
+    completed_tasks = Task.objects.filter(status='Complete')
+    context = {'incomplete_tasks':incomplete_tasks, 'completed_tasks':completed_tasks}
+    return render(request, 'todolist_app/tasklist.html', context)
